3fa673ef7f204df153cbcb911476544813dd0f94
Tal <tzilkha@Tals-MacBook-Pro-2.local>
2022-10-22 15:04:25 +0300

    feat: utility functions - hexToFr

--- tazmania/contract/src/utils.rs
+++ tazmania/contract/src/utils.rs
@@ -1,1 +1,14 @@
-utils.rs
+use ff::*;
+use mimc_sponge_rs::Fr;
+use uint::construct_uint;
+
+construct_uint! {
+    /// 256-bit unsigned integer.
+    pub struct U256(4);
+}
+
+// hex str -> Fr
+pub fn hex_to_fr(hex: &str) -> Fr {
+    let n = hex.parse::<U256>().unwrap().to_string();
+    return Fr::from_str(n.as_str()).unwrap();
+}
